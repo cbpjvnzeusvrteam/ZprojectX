33f1976d524c7365f0781fd06d13150aeb6f7588
cbpjvnzeusvrteam <duongcongbang.dev@gmail.com>
2025-07-10 01:16:31 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ logging.basicConfig(
 
 # --- Cấu hình chung ---
 # Lấy BOT_TOKEN từ biến môi trường, hoặc dùng giá trị mặc định nếu không có (chỉ để phát triển)
-TOKEN = os.environ.get("BOT_TOKEN", "5819094246:AAENFBF2B2dyXLITmEC2ccgLYim2t9vxOQk") # THAY BẰNG TOKEN BOT CỦA BẠN
+TOKEN = os.environ.get("BOT_TOKEN", "7539540916:AAENFBF2B2dyXLITmEC2ccgLYim2t9vxOQk") # THAY BẰNG TOKEN BOT CỦA BẠN
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456789)) # THAY BẰNG ID ADMIN CỦA BẠN
 
 # Đảm bảo APP_URL là URL thuần túy, không có Markdown
